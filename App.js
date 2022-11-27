f26ec30e8631ed459c8f32db62de9b38b740fb5b
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2022-11-27 01:38:54 -0300

    Login succsesfull

--- App.js
+++ App.js
@@ -1,33 +1,61 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'
 import { initializeApp } from 'firebase/app'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
-import { firebaseConfig } from './src/config/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVSRfIED7vsvqXtnVfItNvjKpamWH-nSo",
+  authDomain: "login-auth-5fd49.firebaseapp.com",
+  projectId: "login-auth-5fd49",
+  storageBucket: "login-auth-5fd49.appspot.com",
+  messagingSenderId: "1020974522616",
+  appId: "1:1020974522616:web:ba05023719a49dc242db0d",
+  measurementId: "G-J5RSC53VEG"
+}
 
 export default function App () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const firebaseApp = initializeApp(firebaseConfig)
   const auth = getAuth(firebaseApp)
+
+  const createAcount = () => {
+    console.log(email)
+    console.log(password)
+    
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+          console.log('User created')
+          const user = userCredentials.user
+          console.log(user.email)
+        })
+      .catch((error) => {
+        console.log('error')
+        Alert.alert(error.message)
+      })
+  }
 
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
       <Text style={styles.title}>Welcome</Text>
       <Text style={styles.subtitle}>Sign in with your email account</Text>
-      <Input placeholder='Write your email' />
-      <Input placeholder='Write your password' />
-      <TouchableOpacity >
+      <TextInput
+        placeholder={'Write your email address'}
+        style={styles.input}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder={'Write your password'}
+        style={styles.input}
+        onChangeText= {setPassword}
+      />
+      <TouchableOpacity onPress={createAcount}>
         <Text>Login</Text>
       </TouchableOpacity>
     </View>
-  )
-}
-
-function Input ({ placeholder }) {
-  return (
-    <TextInput
-      placeholder={placeholder}
-      style={styles.input}
-    />
   )
 }
 
